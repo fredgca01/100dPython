62207aa2a22443ffa118597db740eab4b472a638
fredgca01 <fredgca@gmail.com>
2022-07-16 23:48:25 +0200

    day5

---
+++ day5/Loop.py
@@ -0,0 +1,14 @@
+fruits = ["apple","banana","pineapple"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit+" pie")
+print(fruits)
+
+
+for number in range(0,15,3):
+    print(number)
+
+total=0
+for number in range (1,101):
+    total+=number
+print(total)
