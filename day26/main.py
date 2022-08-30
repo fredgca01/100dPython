49bc4ddd07ddcc706fc72e896fa49005e309cdeb
fredgca01 <fredgca@gmail.com>
2022-08-30 22:32:49 +0200

    day26

--- day26/main.py
+++ day26/main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 numbers=[1,2,3]
 new_numbers=[n+1 for n in numbers]
 print(new_numbers)
@@ -20,3 +22,59 @@ print(squared_numbers)
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 result = [number for number in numbers if number%2==0]
 print (result)
+
+names = ["Roger","Jeremy","Emmanuel","Fred"]
+student_scores={student:randint(0,100) for student in names}
+print(student_scores)
+passed_students={student:score for (student,score) in student_scores.items() if score>=50 }
+print(passed_students)
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+words = sentence.split(" ")
+words_size={word:len(word) for word in words}
+print(words_size)
+
+weather = {"Monday":12,
+"Tuesday":14,
+"Wednesday":15,
+"Thursday":14,
+"Friday":21,
+"Saturday":22,
+"Sunday":24}
+
+weather_f = {day:temp*9/5+32 for (day,temp) in weather.items()}
+print(weather_f)
+
+student_dict = {
+    "names":['Roger','Jeremy', 'Emmanuel', 'Fred'],
+    "score":[42,67,54,98]
+}
+
+for (keys,values) in student_dict.items():
+    print(keys)
+    print(values)
+
+import pandas
+
+student_dataframe = pandas.DataFrame(student_dict)
+print(student_dataframe)
+
+for (index,row) in student_dataframe.iterrows():
+    if row.names=="Fred":
+        print(row.score)
+
+nato_data = pandas.read_csv("./day26/nato_phonetic_alphabet.csv")
+sentence = "Fred"
+nato_dict={row.letter:row.code for (index,row) in nato_data.iterrows()}
+#for (index,row) in nato_data.iterrows():
+#    nato_dict[row.letter]=row.code
+#for letter in sentence:
+#    code = nato_dict.get(letter.upper())
+#    print(code)
+
+coded_sentence=[nato_dict.get(letter.upper()) for letter in sentence]
+print(coded_sentence)
+
+
+
+
