7b354628aa2730d6f469839d6ddfa608822217ec
fredgca01 <fredgca@gmail.com>
2022-09-17 14:34:08 +0200

    day29

--- day29/pwd_manager.py
+++ day29/pwd_manager.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -29,16 +30,45 @@ def generate_pwd() -> None:
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save() -> None :
     if len(website_entry.get())!=0 or len(pwd_entry.get())!=0:
-        if messagebox.askyesno(message="Do you want to save this new entry ?"):
-            with open("./day29/data.txt",mode="a") as file:
-                file.write(f"{website_entry.get()}||{login_entry.get()}||{pwd_entry.get()}\n")
-            website_entry.delete(0,END)
+        if messagebox.askyesno(title="Question",message="Do you want to save this new entry ?"):
+            data=None
+            try:
+                with open("./day29/data.json",mode="r") as file:
+                    data =json.load(file)
+            except FileNotFoundError:
+                with open("./day29/data.json",mode="a") as file:
+                    data={website_entry.get():{"login":login_entry.get(),"pwd":pwd_entry.get()}}
+            else:
+                data.update({website_entry.get():{"login":login_entry.get(),"pwd":pwd_entry.get()}})
+            with open("./day29/data.json",mode="w") as file:  
+                json.dump(data,file,indent=4,sort_keys=True)
+                website_entry.delete(0,END)
     else :
         messagebox.showerror(title="oooops !", message="Cannot save empty message")
     pwd_entry.delete(0,END)
     website_entry.focus()
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+def search():
+    website = website_entry.get()
+    if len(website)==0:
+        messagebox.showerror(title="search",message="no website to search")
+    else:
+        data=None
+        try:
+            with open("./day29/data.json",mode="r") as file:
+                data =json.load(file)
+        except FileNotFoundError:
+            messagebox.showerror(title="Search",message="data file not found")
+        else :
+            try :
+                login=data[website]["login"]
+                pwd=data[website]["pwd"]
+                messagebox.showinfo(title="Search", message=f"login:{login}\n pwd:{pwd}")
+            except KeyError as msg:
+                messagebox.showerror(title="Search", message=f"{msg} doesn't exist in your file")
+            
 
 window = Tk()
 window.title("Password manager")
@@ -53,9 +83,11 @@ canvas.grid(row=0,column=1)
 Label(text="Website:",fg="black",bg="white").grid(row=1,column=0)
 Label(text="email/username:",fg="black",bg="white").grid(row=2,column=0)
 Label(text="Password:",fg="black",bg="white").grid(row=3,column=0)
-website_entry = Entry(width=35)
+website_entry = Entry(width=21)
 website_entry.grid(row=1,column=1, columnspan=2)
 website_entry.focus()
+search_button = Button(text="Search",width=10, command=search)
+search_button.grid(row=1,column=3)
 login_entry = Entry(width=35)
 login_entry.grid(row=2,column=1, columnspan=2)
 login_entry.insert(INSERT,"fredgca@gmail.com")
@@ -65,8 +97,6 @@ pwd_button = Button(text="Generate",width=30, command=generate_pwd)
 pwd_button.grid(row=3,column=2)
 add_button = Button(text="Add",width=30, command=save)
 add_button.grid(row=4,column=1, columnspan=2)
-pwd_button = Button(text="Generate",width=30, command=generate_pwd)
-pwd_button.grid(row=3,column=2, columnspan=2)
 
 
 window.mainloop()
