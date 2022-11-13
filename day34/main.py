57e7dd4cd4714a4ebdce59b8341393f299cc0f40
fredgca01 <fredgca@gmail.com>
2022-11-13 16:59:52 +0100

    day34

--- day34/main.py
+++ day34/main.py
@@ -1,3 +1,4 @@
+from ui import QuizzInterface
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
@@ -9,11 +10,5 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
 quiz = QuizBrain(question_bank)
-
-while quiz.still_has_questions():
-    quiz.next_question()
-
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+ui = QuizzInterface(quiz)
