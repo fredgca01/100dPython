18bef31b85404d4013ac2bb16d56e05dac27bd65
fredgca01 <fredgca@gmail.com>
2022-12-30 00:08:00 +0100

    day38

---
+++ day38/main.py
@@ -0,0 +1,38 @@
+import requests
+from datetime import datetime
+import os
+
+NUTRI_KEY=os.getenv("NUTRI_KEY")
+NUTRI_ID=os.getenv("NUTRI_ID")
+NUTRI_URL="https://trackapi.nutritionix.com/v2/natural/exercise"
+SHEETY_URL="https://api.sheety.co/1658c67683f7eb7e1940e0a85f25f886/copieDeMyWorkouts/workouts"
+SHEETY_BEAR=os.getenv("SHEETY_BEAR")
+
+headers={
+    "x-app-id":NUTRI_ID,
+    "x-app-key":NUTRI_KEY,
+    "x-remote-user-id":"0"
+}
+
+query = input("What exercize did you do today ? ")
+
+exercize_param = {
+ "query": query
+}
+
+response = requests.post(url=NUTRI_URL,json=exercize_param,headers=headers)
+
+headers = {"Authorization": SHEETY_BEAR}
+for data in response.json()["exercises"] :
+    data = {
+        "workout": {
+            "date": datetime.now().strftime("%x"),
+            "time": datetime.now().strftime("%X"),
+            "exercise": data["name"],
+            "duration": data["duration_min"],
+            "calories": data["nf_calories"]
+        }
+    }
+    response = requests.post(url=SHEETY_URL,json=data, headers=headers)
+    print(response.text)
+
