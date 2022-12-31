f2a7d4379778b3eb0df197c77a79dc80205e51b7
fredgca01 <fredgca@gmail.com>
2022-12-31 14:17:30 +0100

    add a break on WE

--- day35/main.py
+++ day35/main.py
@@ -1,12 +1,15 @@
 from email.mime.text import MIMEText
 import requests
-import json
 import os
 import smtplib
+from datetime import datetime
 
 API_KEY=os.environ.get("OWM_API_KEY")
 URL_LOC="http://api.openweathermap.org/geo/1.0/direct"
 URL_WEATHER = "https://api.openweathermap.org/data/2.5/forecast"
+MY_EMAIL= os.environ.get("MY_MAIL")
+MAIL_PWD= os.environ.get("MAIL_PWD")
+EMAILS=os.environ.get("EMAILS")
 
 PARAMS_LOC = {
     "q":"Paris,FR",
@@ -14,103 +17,61 @@ PARAMS_LOC = {
     "appid":API_KEY
 }
 
-response = requests.get(url=URL_LOC, params=PARAMS_LOC)
-response.raise_for_status()
-lat = response.json()[0]["lat"]
-lon = response.json()[0]["lon"]
+if datetime.now().weekday()==5 or datetime.now().weekday()==6:
+    print("Weekend")
+else:
 
-PARAMS_WEATHER = {
-    "lat":lat,
-    "lon":lon,
-    "appid":API_KEY,
-    "units":"metric",
-    "lang":"FR",
-    "cnt":3
-}
+    response = requests.get(url=URL_LOC, params=PARAMS_LOC)
+    response.raise_for_status()
+    lat = response.json()[0]["lat"]
+    lon = response.json()[0]["lon"]
 
-response = requests.get(url=URL_WEATHER, params=PARAMS_WEATHER)
-response.raise_for_status()
-data = response.json()["list"]
-raining = False
-weather_alert=""
-temp_feel=0
+    PARAMS_WEATHER = {
+        "lat":lat,
+        "lon":lon,
+        "appid":API_KEY,
+        "units":"metric",
+        "lang":"FR",
+        "cnt":3
+    }
 
-for weather_slot in data:
-    detailled_temp = weather_slot["main"]
-    temp_feel += float(detailled_temp.get("feels_like"))
-    detailled_weather = weather_slot["weather"]
-    weather_type = int(detailled_weather[0].get("id"))
-    icon_txt = detailled_weather[0].get("icon")
-    icon_url = "http://openweathermap.org/img/wn/"+icon_txt+"@2x.png"
-    if weather_type>=500 and weather_type<521 and not raining:
-        weather_alert = f"Penses au parapluie: {icon_url}\n "
-        raining=True
-    elif weather_type>=200 and weather_type<300 and not raining:
-        weather_alert = f"Il vaudrait mieux ne pas sortir en fait, orage attendu ...{icon_url}\n"
-        raining=True
-    elif weather_type>=521 and weather_type<600 and not raining:
-        weather_alert = f"Il vaudrait mieux ne pas sortir en fait, ca va tomber dru ...{icon_url}\n "
-        raining=True
+    response = requests.get(url=URL_WEATHER, params=PARAMS_WEATHER)
+    response.raise_for_status()
+    data = response.json()["list"]
+    raining = False
+    weather_alert=""
+    temp_feel=0
 
-temp_feel=temp_feel/3
-weather_alert+=f"La temperature sera de: {round(temp_feel,1)}°"
-print(weather_alert)
+    for weather_slot in data:
+        detailled_temp = weather_slot["main"]
+        temp_feel += float(detailled_temp.get("feels_like"))
+        detailled_weather = weather_slot["weather"]
+        weather_type = int(detailled_weather[0].get("id"))
+        icon_txt = detailled_weather[0].get("icon")
+        icon_url = "http://openweathermap.org/img/wn/"+icon_txt+"@2x.png"
+        if weather_type>=500 and weather_type<521 and not raining:
+            weather_alert = f"Penses au parapluie: {icon_url}\n "
+            raining=True
+        elif weather_type>=200 and weather_type<300 and not raining:
+            weather_alert = f"Il vaudrait mieux ne pas sortir en fait, orage attendu ...{icon_url}\n"
+            raining=True
+        elif weather_type>=521 and weather_type<600 and not raining:
+            weather_alert = f"Il vaudrait mieux ne pas sortir en fait, ca va tomber dru ...{icon_url}\n "
+            raining=True
 
-MY_EMAIL= os.environ.get("MY_MAIL")
-MAIL_PWD= os.environ.get("MAIL_PWD")
-EMAILS=os.environ.get("EMAILS")
+    temp_feel=temp_feel/3
+    weather_alert+=f"La temperature sera de: {round(temp_feel,1)}°"
+    print(weather_alert)
 
-def send_email(to,subject, body):
-    with smtplib.SMTP(host="smtp.gmail.com",port=587) as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL,password=MAIL_PWD)
-        #connection.sendmail(from_addr=MY_EMAIL,to_addrs=to,msg=f"Subject:{subject}\n\n{body}")
-        message = MIMEText(body)
-        message["To"]=EMAILS
-        message["From"]=MY_EMAIL
-        message["Subject"]=subject
-        connection.send_message(message)
+    def send_email(to,subject, body):
+        with smtplib.SMTP(host="smtp.gmail.com",port=587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL,password=MAIL_PWD)
+            #connection.sendmail(from_addr=MY_EMAIL,to_addrs=to,msg=f"Subject:{subject}\n\n{body}")
+            message = MIMEText(body)
+            message["To"]=to
+            message["From"]=MY_EMAIL
+            message["Subject"]=subject
+            connection.send_message(message)
 
-send_email(MY_EMAIL,"Meteo",weather_alert)
-
-# # https://business.facebook.com/settings/system-users/100087678714205?business_id=5431964390266258
-# # https://developers.facebook.com/docs/whatsapp/cloud-api/get-started
-
-# phone_number_id = os.environ.get("WHTSP_PHONE_ID") # Phone number ID provided
-# access_token=os.environ.get("WHTSP_ACCESS_TOKEN") 
-# recipient_phone_number = "33662251612" # Your own phone number
-# recipient_phone_number2 = "33664626482"
-# url = f"https://graph.facebook.com/v15.0/{phone_number_id}/messages"
-# headers = {
-#     "Authorization": f"Bearer {access_token}",
-#     'Content-Type': 'application/json'
-# }
-
-# # https://developers.facebook.com/docs/whatsapp/cloud-api/guides/send-messages#text-messages
-# parameters_tmpl = {
-#     "messaging_product": "whatsapp",
-#     "recipient_type": "individual",
-#     "to": recipient_phone_number,
-#     "type": "template",
-#     "template": {"name": "hello_world", "language": {"code": "en_US"}}
-# }
-
-# # il faut que le receveur reponde à un msg avant de pouvoir recevoir autre chose que le template hello world ...
-# parameters_txt = {
-#     "messaging_product": "whatsapp",
-#     "recipient_type": "individual",
-#     "to": recipient_phone_number2,
-#     "type": "text",
-#     "text": {
-#         "preview_url": 'true',
-#         "body": weather_alert
-#     }
-# }
-
-# response = requests.post(
-#     url,
-#     headers=headers,
-#     data=json.dumps(parameters_txt)
-# )
-
-# response.raise_for_status()
+    send_email(EMAILS,"Meteo",weather_alert)
